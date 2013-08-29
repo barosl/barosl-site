2c6592c10a2c2d051ac6ec4ea1eb26e740e2a280
Barosl LEE <vcs@barosl.com>
2013-08-29 23:28:11 +0900

    views.py: Add uptime information to the main page

--- index/views.py
+++ index/views.py
@@ -1,6 +1,8 @@
 from . import app
 import flask
+import datetime
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    uptime = str(datetime.timedelta(seconds=int(float(open('/proc/uptime').read().split()[0]))))
+    return flask.render_template('index.html', uptime=uptime)
